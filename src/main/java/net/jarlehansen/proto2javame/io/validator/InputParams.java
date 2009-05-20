072c839f3bc3bc97b33700faeae4ef7248f93b05
Jarle Hansen <hansjar@gmail.com>
2009-05-20 15:39:23 +0000

    initial import

---
+++ src/main/java/net/jarlehansen/proto2javame/io/validator/InputParams.java
@@ -0,0 +1,13 @@
+package net.jarlehansen.proto2javame.io.validator;
+
+/**
+ * 
+ * @author hansjar@gmail.com Jarle Hansen
+ *
+ */
+enum InputParams {
+	;
+	
+	public static final String JAVA_OUT_TAG = "--java_out=";
+	public static final int VALID_NUMBER_OF_PARAMS = 2;
+}
