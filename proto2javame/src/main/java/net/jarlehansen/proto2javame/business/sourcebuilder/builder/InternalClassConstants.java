5753b033fbddb65fa5e89a887311ad5eb275d20a
Jarle Hansen <hansjar@gmail.com>
2009-05-20 16:49:09 +0000

    initial import

---
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/builder/InternalClassConstants.java
@@ -0,0 +1,24 @@
+package net.jarlehansen.proto2javame.business.sourcebuilder.builder;
+
+/**
+ * 
+ * @author hansjar@gmail.com Jarle Hansen
+ *
+ */
+enum InternalClassConstants {
+	;
+	
+	public static final String KEY_BUILDER_INIT = "internal.builder.init";
+	
+	public static final String KEY_BUILDER_FIELDS = "internal.builder.fields";
+	public static final String KEY_BUILDER_FIELDS_LIST = "internal.builder.fields.list";
+	public static final String KEY_BUILDER_FIELDS_BOOL = "internal.builder.fields.bool";
+	
+	public static final String KEY_BUILDER_CONSTRUCTOR = "internal.builder.constructor";
+	
+	public static final String KEY_BUILDER_METHODS = "internal.builder.methods";
+	public static final String KEY_BUILDER_METHODS_LIST = "internal.builder.methods.list";
+	public static final String KEY_BUILDER_BUILD_METHOD = "internal.builder.build.method";
+	
+	public static final String KEY_BUILDER_END = "internal.builder.end";
+}
