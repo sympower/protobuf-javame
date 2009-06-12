ca38134892000acdf39e91b47552b5231c690e39
Jarle Hansen <hansjar@gmail.com>
2009-06-12 10:21:43 +0000

    added tests for new line.separator logic

--- proto2javame/src/test/java/net/jarlehansen/proto2javame/business/sourcebuilder/resource/ResourceFormatUtilTest.java
+++ proto2javame/src/test/java/net/jarlehansen/proto2javame/business/sourcebuilder/resource/ResourceFormatUtilTest.java
@@ -1,13 +1,13 @@
 package net.jarlehansen.proto2javame.business.sourcebuilder.resource;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 import java.util.MissingResourceException;
 
 /**
  * @author Jarle Hansen hansjar@gmail.com
- * Date: May 14, 2009
+ *         Date: May 14, 2009
  */
 public class ResourceFormatUtilTest {
 
@@ -29,5 +29,63 @@ public class ResourceFormatUtilTest {
     @Test(expected = MissingResourceException.class)
     public void testGetStringVarArgsInvalidInput() {
         ResourceFormatUtil.BUILDER.getString("not a valid key");
+    }
+
+    @Test
+    public void testLineSeparatorWindows() {
+        ResourceFormatUtil.BUILDER.lineSeparator = "\r\n";
+
+        final String resourceString = ResourceFormatUtil.BUILDER.getString("internal.builder.init");
+
+        final char[] chars = resourceString.toCharArray();
+
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            sb.append(Integer.toHexString(chars[i]));
+        }
+
+        final String lastLineSeparator = sb.toString().toLowerCase().substring(sb.toString().length() - 2);
+
+        // The Windows line separator is 0d 0a, but hexString will remove 0's. So the result should be 'da'
+        assertEquals("da", lastLineSeparator);
+    }
+
+    @Test
+    public void testLineSeparatorUnix() {
+        ResourceFormatUtil.BUILDER.lineSeparator = "\n";
+
+        final String resourceString = ResourceFormatUtil.BUILDER.getString("internal.builder.init");
+
+        final char[] chars = resourceString.toCharArray();
+
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            sb.append(Integer.toHexString(chars[i]));
+        }
+
+        final String lastLineSeparator = sb.toString().toLowerCase().substring(sb.toString().length() - 2);
+
+        // The Unix line separator is 0a, but hexString will remove 0. So the result should be 'a'
+        assertNotSame("d", lastLineSeparator.substring(0, 1));
+        assertEquals("a", lastLineSeparator.substring(1));
+    }
+
+    @Test
+    public void testLineSeparatorOSX() {
+        ResourceFormatUtil.BUILDER.lineSeparator = "\r";
+
+        final String resourceString = ResourceFormatUtil.BUILDER.getString("internal.builder.init");
+
+        final char[] chars = resourceString.toCharArray();
+
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chars.length; i++) {
+            sb.append(Integer.toHexString(chars[i]));
+        }
+
+        final String lastLineSeparator = sb.toString().toLowerCase().substring(sb.toString().length() - 1);
+
+        // The Unix line separator is 0d, but hexString will remove 0. So the result should be 'd'
+        assertEquals("d", lastLineSeparator);
     }
 }
