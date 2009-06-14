d7192d0f6e9e0ce2221460e78c3dcb1bfdf49b1f
Jarle Hansen <hansjar@gmail.com>
2009-06-14 19:15:47 +0000

    restructured the import properties

--- proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/resource/CommonResourceConstants.java
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/resource/CommonResourceConstants.java
@@ -7,9 +7,12 @@ package net.jarlehansen.proto2javame.business.sourcebuilder.resource;
  */
 public enum CommonResourceConstants {
 	;
-	
-	public static final String KEY_IMPORTS_INTERNAL_PACKAGE = "imports.internal.package.structure";
-	public static final String KEY_IMPORTS_GOOGLE_PACKAGE = "imports.google.package.structure";
+
+    public static final String KEY_IMPORTS_PACKAGE_MAIN = "imports.package.structure.main";
+    public static final String KEY_IMPORTS_PACKAGE_INPUT = "imports.package.structure.input";
+    public static final String KEY_IMPORTS_PACKAGE_OUTPUT = "imports.package.structure.output";
+    public static final String KEY_IMPORTS_PACKAGE_UTIL = "imports.package.structure.util";
+    public static final String KEY_IMPORTS_PACKAGE_FACTORY = "imports.package.structure.factory";
 	
 	public static final String KEY_SUPPORTED_DATA_TYPES = "class.supported.data.types";
 	public static final String KEY_COMPUTE_SIZE_UTIL = "class.compute.size.util";
