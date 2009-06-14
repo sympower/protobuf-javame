5a89a7d05140686f67390fe74e1926b6c772393a
Jarle Hansen <hansjar@gmail.com>
2009-06-14 19:41:05 +0000

    added tests

--- proto2javame/src/test/java/net/jarlehansen/proto2javame/protobuf/integration/GoogleProtobufIntegration.java
+++ proto2javame/src/test/java/net/jarlehansen/proto2javame/protobuf/integration/GoogleProtobufIntegration.java
@@ -1,6 +1,8 @@
 package net.jarlehansen.proto2javame.protobuf.integration;
 
 import net.jarlehansen.proto2javame.generated.sourcecode.tests.tempfiles.JunitTestMainObject;
+import net.jarlehansen.proto2javame.protobuf.integration.tempfiles.JunitTestMainObjectProto;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,14 +13,26 @@ import java.io.IOException;
  *         Time: 7:44:38 PM
  */
 public class GoogleProtobufIntegration {
+    private static final int ID = 123987;
 
     @Test
     public void testJavaMeGeneratedByteArrayToStandardProtobuf() throws IOException {
-        final JunitTestMainObject javaMeObj = JunitTestMainObject.newBuilder().setId(123).build();
+        final JunitTestMainObject javaMeObj = JunitTestMainObject.newBuilder().setId(ID).build();
 
         final byte[] javaMeData = javaMeObj.toByteArray();
 
-        
+        final JunitTestMainObjectProto.JunitTestMainObject javaSeObj = JunitTestMainObjectProto.JunitTestMainObject.parseFrom(javaMeData);
+        assertEquals(ID, javaSeObj.getId());
+    }
 
+    @Test
+    public void testJavaSeGeneratedByteArrayToJavaMeProtobuf() throws IOException {
+        final JunitTestMainObjectProto.JunitTestMainObject javaSeObj = JunitTestMainObjectProto.JunitTestMainObject
+                .newBuilder().setId(ID).build();
+
+        final byte[] javaSeData = javaSeObj.toByteArray();
+
+        final JunitTestMainObject javaMeObj = JunitTestMainObject.parseFrom(javaSeData);
+        assertEquals(ID, javaMeObj.getId());
     }
 }
