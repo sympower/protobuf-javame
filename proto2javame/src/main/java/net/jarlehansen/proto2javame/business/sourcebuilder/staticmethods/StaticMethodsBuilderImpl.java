e399125e73c6931a7faef926acf87a0ee47a7fad
Jarle Hansen <hansjar@gmail.com>
2009-07-02 20:55:21 +0000

    added createSetUnknownTagHandler(className)

--- proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/staticmethods/StaticMethodsBuilderImpl.java
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/staticmethods/StaticMethodsBuilderImpl.java
@@ -3,38 +3,41 @@ package net.jarlehansen.proto2javame.business.sourcebuilder.staticmethods;
 import net.jarlehansen.proto2javame.business.sourcebuilder.resource.ResourceFormatUtil;
 
 /**
- * 
  * @author hansjar@gmail.com Jarle Hansen
- *
  */
 public final class StaticMethodsBuilderImpl implements StaticMethodsBuilder {
-	private final ResourceFormatUtil resourceFormat = ResourceFormatUtil.STATIC_METHODS;
-	
-	private StaticMethodsBuilderImpl() {
-	}
+    private final ResourceFormatUtil resourceFormat = ResourceFormatUtil.STATIC_METHODS;
 
-	public static StaticMethodsBuilder newInstance() {
-		return new StaticMethodsBuilderImpl();
-	}
+    private StaticMethodsBuilderImpl() {
+    }
 
-	public StringBuilder createStaticMethods(final String className) {
-		final StringBuilder builder = new StringBuilder();
-		builder.append(createParseFromByteArray(className));
-		builder.append(createParseFromInputStream(className));
-		builder.append(createClassEnd());
+    public static StaticMethodsBuilder newInstance() {
+        return new StaticMethodsBuilderImpl();
+    }
 
-		return builder;
-	}
+    public StringBuilder createStaticMethods(final String className) {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(createSetUnknownTagHandler(className));
+        builder.append(createParseFromByteArray(className));
+        builder.append(createParseFromInputStream(className));
+        builder.append(createClassEnd());
 
-	private String createParseFromByteArray(final String className) {
-		return resourceFormat.getString(StaticMethodsConstants.KEY_STATIC_PARSEFROM_BYTEARRAY, className);
-	}
+        return builder;
+    }
 
-	private String createParseFromInputStream(final String className) {
-		return resourceFormat.getString(StaticMethodsConstants.KEY_STATIC_PARSEFROM_INPUTSTREAM, className);
-	}
-	
-	private String createClassEnd() {
-		return resourceFormat.getString(StaticMethodsConstants.KEY_CLASS_END);
-	}
+    private String createSetUnknownTagHandler(final String className) {
+        return resourceFormat.getString(StaticMethodsConstants.KEY_STATIC_SET_UNKNOWNTAGHANDLER, className);
+    }
+
+    private String createParseFromByteArray(final String className) {
+        return resourceFormat.getString(StaticMethodsConstants.KEY_STATIC_PARSEFROM_BYTEARRAY, className);
+    }
+
+    private String createParseFromInputStream(final String className) {
+        return resourceFormat.getString(StaticMethodsConstants.KEY_STATIC_PARSEFROM_INPUTSTREAM, className);
+    }
+
+    private String createClassEnd() {
+        return resourceFormat.getString(StaticMethodsConstants.KEY_CLASS_END);
+    }
 }
