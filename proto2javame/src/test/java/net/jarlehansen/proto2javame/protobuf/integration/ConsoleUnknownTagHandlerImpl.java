1a87fd0f6734b7135fe95f8f6474c29fed3d8bf7
Jarle Hansen <hansjar@gmail.com>
2009-07-02 20:45:28 +0000

    test class that will save the UnknownTagHandler String created by the protobuf-javame library and print it on the console. Used in the integration tests.

---
+++ proto2javame/src/test/java/net/jarlehansen/proto2javame/protobuf/integration/ConsoleUnknownTagHandlerImpl.java
@@ -0,0 +1,21 @@
+package net.jarlehansen.proto2javame.protobuf.integration;
+
+import net.jarlehansen.protobuf.javame.input.taghandler.UnknownTagHandler;
+
+/**
+ * @author Jarle Hansen hansjar@gmail.com
+ *         Date: Jul 2, 2009
+ *         Time: 8:32:39 PM
+ */
+public class ConsoleUnknownTagHandlerImpl implements UnknownTagHandler{
+    private String unknownFields = "";
+
+    public void unknownTag(final String unknownFields) {
+        this.unknownFields = unknownFields;
+        System.out.println(unknownFields);
+    }
+
+    public String getUnknownFields() {
+        return unknownFields;
+    }
+}
