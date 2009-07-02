050083631bbab81debbfa03c15ad521c292b81e1
Jarle Hansen <hansjar@gmail.com>
2009-07-02 20:55:39 +0000

    added KEY_STATIC_SET_UNKNOWNTAGHANDLER

--- proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/staticmethods/StaticMethodsConstants.java
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/staticmethods/StaticMethodsConstants.java
@@ -7,7 +7,8 @@ package net.jarlehansen.proto2javame.business.sourcebuilder.staticmethods;
  */
 enum StaticMethodsConstants {
 	;
-	
+
+    public static final String KEY_STATIC_SET_UNKNOWNTAGHANDLER = "static.set.unknowntaghandler";
 	public static final String KEY_STATIC_PARSEFROM_BYTEARRAY = "static.parsefrom.bytearray";
 	public static final String KEY_STATIC_PARSEFROM_INPUTSTREAM = "static.parsefrom.inputstream";
 	public static final String KEY_CLASS_END = "class.end";
