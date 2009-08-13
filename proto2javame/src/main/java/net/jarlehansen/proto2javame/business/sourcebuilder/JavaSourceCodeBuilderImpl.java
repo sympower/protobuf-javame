03d2a5ae9ca3b89628e44c8324b2dd3d16d4b2f6
Jarle Hansen <hansjar@gmail.com>
2009-08-13 19:25:16 +0000

    updated

--- proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/JavaSourceCodeBuilderImpl.java
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/JavaSourceCodeBuilderImpl.java
@@ -3,7 +3,7 @@ package net.jarlehansen.proto2javame.business.sourcebuilder;
 import net.jarlehansen.proto2javame.business.sourcebuilder.builder.InternalClassBuilder;
 import net.jarlehansen.proto2javame.business.sourcebuilder.factory.JavaSourceCodeBuilderFactory;
 import net.jarlehansen.proto2javame.business.sourcebuilder.main.MainClassBuilder;
-import net.jarlehansen.proto2javame.business.sourcebuilder.privatemethods.PrivateMethodsBuilder;
+import net.jarlehansen.proto2javame.business.sourcebuilder.privateandprotectedmethods.PrivateAndProtectedMethodsBuilder;
 import net.jarlehansen.proto2javame.business.sourcebuilder.publicmethods.PublicMethodsBuilder;
 import net.jarlehansen.proto2javame.business.sourcebuilder.resource.JavaSourceCodeUtil;
 import net.jarlehansen.proto2javame.business.sourcebuilder.staticmethods.StaticMethodsBuilder;
@@ -18,7 +18,7 @@ import net.jarlehansen.proto2javame.domain.proto.ProtoFileInput;
 public final class JavaSourceCodeBuilderImpl implements JavaSourceCodeBuilder {
 	private final InternalClassBuilder internalClassBuilder;
 	private final MainClassBuilder mainClassBuilder;
-	private final PrivateMethodsBuilder privateMethodsBuilder;
+	private final PrivateAndProtectedMethodsBuilder privateMethodsBuilder;
 	private final PublicMethodsBuilder publicMethodsBuilder;
 	private final StaticMethodsBuilder staticMethodsBuilder;
 
@@ -40,7 +40,7 @@ public final class JavaSourceCodeBuilderImpl implements JavaSourceCodeBuilder {
 
 		javaOutput.setMainClass(mainClassBuilder.createMainClass(javaOutput.getClassName(), protoInput));
 		javaOutput.setInternalClass(internalClassBuilder.createInternalClass(javaOutput.getClassName(), protoInput));
-		javaOutput.setPrivateMethods(privateMethodsBuilder.createPrivateMethods(javaOutput.getClassName(), protoInput));
+		javaOutput.setPrivateAndProtectedMethods(privateMethodsBuilder.createPrivateAndProtectedMethods(javaOutput.getClassName(), protoInput));
 		javaOutput.setPublicMethods(publicMethodsBuilder.createPublicMethods(javaOutput.getClassName(), protoInput));
 		javaOutput.setStaticMethods(staticMethodsBuilder.createStaticMethods(javaOutput.getClassName()));
 		
