5b774730ff222b386c07c3b278aaac121c850e77
Jarle Hansen <hansjar@gmail.com>
2009-08-13 19:32:38 +0000

    added javaObjectType and primitiveType

--- proto2javame/src/main/java/net/jarlehansen/proto2javame/domain/proto/ValidTypes.java
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/domain/proto/ValidTypes.java
@@ -1,27 +1,43 @@
 package net.jarlehansen.proto2javame.domain.proto;
 
+import net.jarlehansen.proto2javame.business.sourcebuilder.resource.JavaSourceCodeUtil;
+
 /**
  * 
  * @author hansjar@gmail.com Jarle Hansen
  *
  */
 public enum ValidTypes {
-	DOUBLE("double", "double", "DATA_TYPE_DOUBLE"), 
-	FLOAT("float", "float", "DATA_TYPE_FLOAT"), 
-	INT32("int32", "int", "DATA_TYPE_INT"), 
-	INT64("int64", "long", "DATA_TYPE_LONG"), 
-	BOOL("bool", "boolean", "DATA_TYPE_BOOLEAN"),
-	STRING("string", "String", "DATA_TYPE_STRING"),
-	BYTES("bytes", "net.jarlehansen.protobuf.javame.ByteString", "DATA_TYPE_BYTESTRING");
+	DOUBLE("double", "double", "DATA_TYPE_DOUBLE", true),
+	FLOAT("float", "float", "DATA_TYPE_FLOAT", true),
+	INT32("int32", "int", "DATA_TYPE_INT", true, "Integer"),
+	INT64("int64", "long", "DATA_TYPE_LONG", true),
+	BOOL("bool", "boolean", "DATA_TYPE_BOOLEAN", true),
+	STRING("string", "String", "DATA_TYPE_STRING", false),
+	BYTES("bytes", "net.jarlehansen.protobuf.javame.ByteString", "DATA_TYPE_BYTESTRING", false);
 	
 	private final String javaType;
 	private final String protoType;
 	private final String dataTypeConstant; // Used to identify list data types
+    private final boolean primitiveType;
+    private final String javaObjectType; // If this is not set, the object type will be the same as the javaType with a capital letter
 	
-	private ValidTypes(final String protoType, final String javaType, final String dataTypeConstant) {
+	private ValidTypes(final String protoType, final String javaType, final String dataTypeConstant, final boolean primitiveType) {
 		this.protoType = protoType;
 		this.javaType = javaType;
 		this.dataTypeConstant = dataTypeConstant;
+        this.primitiveType = primitiveType;
+
+        javaObjectType = JavaSourceCodeUtil.createCapitalLetterName(javaType);
+	}
+
+    private ValidTypes(final String protoType, final String javaType, final String dataTypeConstant, final boolean primitiveType,
+                       final String javaObjectType) {
+		this.protoType = protoType;
+		this.javaType = javaType;
+		this.dataTypeConstant = dataTypeConstant;
+        this.primitiveType = primitiveType;
+        this.javaObjectType = javaObjectType;
 	}
 	
 	public String getJavaType() {
@@ -35,4 +51,12 @@ public enum ValidTypes {
 	public String getDataTypeConstant() {
 		return dataTypeConstant;
 	}
+
+    public boolean isPrimitiveType() {
+        return primitiveType;
+    }
+
+    public String getJavaObjectType() {
+        return javaObjectType;
+    }
 }
