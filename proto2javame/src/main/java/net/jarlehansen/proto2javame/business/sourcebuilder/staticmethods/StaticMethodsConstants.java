86a545e5d5c7199f109751415977d35bc48251dd
Jarle Hansen <hansjar@gmail.com>
2009-08-13 19:31:30 +0000

    added KEY_STATIC_PARSEDELIMITEDFROM_INPUTSTREAM

--- proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/staticmethods/StaticMethodsConstants.java
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/business/sourcebuilder/staticmethods/StaticMethodsConstants.java
@@ -11,5 +11,6 @@ enum StaticMethodsConstants {
     public static final String KEY_STATIC_SET_UNKNOWNTAGHANDLER = "static.set.unknowntaghandler";
 	public static final String KEY_STATIC_PARSEFROM_BYTEARRAY = "static.parsefrom.bytearray";
 	public static final String KEY_STATIC_PARSEFROM_INPUTSTREAM = "static.parsefrom.inputstream";
+    public static final String KEY_STATIC_PARSEDELIMITEDFROM_INPUTSTREAM = "static.parsedelimitedfrom.inputstream";
 	public static final String KEY_CLASS_END = "class.end";
 }
