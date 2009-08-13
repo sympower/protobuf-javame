601e2e883c0e3f6f58f2231702a0f3444c2fdd00
Jarle Hansen <hansjar@gmail.com>
2009-08-13 19:52:26 +0000

    updated

--- protobuf-javame/src/main/java/net/jarlehansen/protobuf/javame/factory/JavaMeProtoFactory.java
+++ protobuf-javame/src/main/java/net/jarlehansen/protobuf/javame/factory/JavaMeProtoFactory.java
@@ -9,29 +9,28 @@ import net.jarlehansen.protobuf.javame.input.taghandler.UnknownTagHandler;
 import net.jarlehansen.protobuf.javame.output.OutputWriter;
 import net.jarlehansen.protobuf.javame.output.OutputWriterImpl;
 
-
 /**
  * 
  * @author hansjar@gmail.com Jarle Hansen
- *
+ * 
  */
 public final class JavaMeProtoFactory {
-	
+
 	private JavaMeProtoFactory() {
 	}
-	
+
 	public static InputReader createInputUtil(final byte[] buffer, final UnknownTagHandler unknownTagHandler) {
 		return new InputReaderImpl(buffer, unknownTagHandler);
 	}
-	
+
 	public static InputReader createInputUtil(final InputStream input, final UnknownTagHandler unknownTagHandler) {
 		return new InputReaderImpl(input, unknownTagHandler);
 	}
-	
+
 	public static OutputWriter createOutputUtil(final byte[] dataHolder) {
 		return new OutputWriterImpl(dataHolder);
 	}
-	
+
 	public static OutputWriter createOutputUtil(final byte[] dataHolder, final OutputStream output) {
 		return new OutputWriterImpl(dataHolder, output);
 	}
