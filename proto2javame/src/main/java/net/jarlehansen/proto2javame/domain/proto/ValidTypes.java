7df8fef91a62b7e12f2b69b25700ab49ca6e097d
Jarle Hansen <hansjar@gmail.com>
2009-08-14 09:46:13 +0000

    removed the depenency to JavaSourceCodeUtil

--- proto2javame/src/main/java/net/jarlehansen/proto2javame/domain/proto/ValidTypes.java
+++ proto2javame/src/main/java/net/jarlehansen/proto2javame/domain/proto/ValidTypes.java
@@ -1,18 +1,16 @@
 package net.jarlehansen.proto2javame.domain.proto;
 
-import net.jarlehansen.proto2javame.business.sourcebuilder.resource.JavaSourceCodeUtil;
-
 /**
  * 
  * @author hansjar@gmail.com Jarle Hansen
  *
  */
 public enum ValidTypes {
-	DOUBLE("double", "double", "DATA_TYPE_DOUBLE", true),
-	FLOAT("float", "float", "DATA_TYPE_FLOAT", true),
+	DOUBLE("double", "double", "DATA_TYPE_DOUBLE", true, "Double"),
+	FLOAT("float", "float", "DATA_TYPE_FLOAT", true, "Float"),
 	INT32("int32", "int", "DATA_TYPE_INT", true, "Integer"),
-	INT64("int64", "long", "DATA_TYPE_LONG", true),
-	BOOL("bool", "boolean", "DATA_TYPE_BOOLEAN", true),
+	INT64("int64", "long", "DATA_TYPE_LONG", true, "Long"),
+	BOOL("bool", "boolean", "DATA_TYPE_BOOLEAN", true, "Boolean"),
 	STRING("string", "String", "DATA_TYPE_STRING", false),
 	BYTES("bytes", "net.jarlehansen.protobuf.javame.ByteString", "DATA_TYPE_BYTESTRING", false);
 	
@@ -27,8 +25,7 @@ public enum ValidTypes {
 		this.javaType = javaType;
 		this.dataTypeConstant = dataTypeConstant;
         this.primitiveType = primitiveType;
-
-        javaObjectType = JavaSourceCodeUtil.createCapitalLetterName(javaType);
+        javaObjectType = "";
 	}
 
     private ValidTypes(final String protoType, final String javaType, final String dataTypeConstant, final boolean primitiveType,
